16bff920e4dac602d5f8d199629d5d1d7789d936
Artem Silka <silkartem@gmail.com>
2023-06-06 22:43:39 -0400

    intro practice

---
+++ practice.py
@@ -0,0 +1,29 @@
+#If-Else
+
+if n % 2 != 0:
+    print("Weird")
+else:
+    if n in range(2,6) or n > 20:
+        print("Not Weird")
+    elif n in range(6,21):
+        print("Weird")
+
+#Arithmetic Operators 
+
+print(f"""{a + b}
+{a - b}
+{a * b}""")
+
+# Python: Division
+
+print(f"""{a // b}
+{a / b}""")
+
+#Loops
+
+for num in range(0, n):
+    print(num ** 2)
+
+#Write a function
+def is_leap(year): 
+    return (year % 4 == 0 and year % 100 == 0 and year % 400 == 0)
