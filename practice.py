459a695c901ab64d63bd5d2eac44cf7aa9756870
Artem Silka <silkartem@gmail.com>
2023-06-08 15:03:17 -0400

    args and kwargs

--- practice.py
+++ practice.py
@@ -1,29 +1,29 @@
-#If-Else
+# #If-Else
 
-if n % 2 != 0:
-    print("Weird")
-else:
-    if n in range(2,6) or n > 20:
-        print("Not Weird")
-    elif n in range(6,21):
-        print("Weird")
+# if n % 2 != 0:
+#     print("Weird")
+# else:
+#     if n in range(2,6) or n > 20:
+#         print("Not Weird")
+#     elif n in range(6,21):
+#         print("Weird")
 
-#Arithmetic Operators 
+# #Arithmetic Operators 
 
-print(f"""{a + b}
-{a - b}
-{a * b}""")
+# print(f"""{a + b}
+# {a - b}
+# {a * b}""")
 
-# Python: Division
+# # Python: Division
 
-print(f"""{a // b}
-{a / b}""")
+# print(f"""{a // b}
+# {a / b}""")
 
-#Loops
+# #Loops
 
-for num in range(0, n):
-    print(num ** 2)
+# for num in range(0, n):
+#     print(num ** 2)
 
-#Write a function
-def is_leap(year): 
-    return (year % 4 == 0 and year % 100 == 0 and year % 400 == 0)
+# #Write a function
+# def is_leap(year): 
+#     return (year % 4 == 0 and year % 100 == 0 and year % 400 == 0)
